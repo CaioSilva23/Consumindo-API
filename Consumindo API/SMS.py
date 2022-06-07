f29af44a66490a7c781eabf9d76d321af82ec295
CaioSilva23 <105164888+CaioSilva23@users.noreply.github.com>
2022-06-06 21:59:12 -0300

    sms

---
+++ Consumindo API/SMS.py
@@ -0,0 +1,18 @@
+from twilio.rest import Client
+import twilio
+
+account_sid = 'ACb7b858e87e438703c5e6471686a81690'  # CONTA TWILLO
+
+token = '66f50c497313ef3010aaadaa577cba82'  # TOKEN DA CONTA TWILLO
+
+remetente = '+13073232371'  # REMETENTE OBS: NÚMERO DA CONTA TWILLO
+destino = '+5521971025283'  # DESTINATÁRIO
+
+client = Client(account_sid, token)
+
+message = client.messages.create(from_=remetente,
+                                 body='KOE CAIO, PYTHON É FODAAA',
+                                 to=destino)
+# ENVIAR O SMS, COM REMETENTE, DESTINATÁRIO E MSG
+
+print(message.sid)
